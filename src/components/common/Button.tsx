ffe6b4077b2ebe3b131b2311e27398abffc0c350
StVictoria <2018vvs@gmail.com>
2021-12-14 12:27:05 +0300

    added: styles for Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,10 +1,11 @@
+import "./Button.scss";
 interface IButton {
     icon?: string,
     title: string
 }
 
 function Button({icon, title}: IButton) {
-    return <button>{icon}{title}</button>    
+    return <button className="Button">{icon}{title}</button>    
 }
 
 export default Button;
