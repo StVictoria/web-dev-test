335a5b5bc9cc46de63ceb037974c295a8aab2cf0
StVictoria <2018vvs@gmail.com>
2021-12-15 16:38:21 +0300

    add: react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,17 @@
-import Header from './base/Header';
-import HomePage from './pages/HomePage';
-import './App.scss';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./base/Header";
+import HomePage from "./pages/HomePage";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-     <Header title="JavaScript & React & Redux" />
+      <Header title="JavaScript & React & Redux" />
       <main className="App-Main">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </main>
     </div>
   );
