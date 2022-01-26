58e1c8f6244e95f418871f99dd2aa5234d4f4481
StVictoria <2018vvs@gmail.com>
2022-01-26 11:39:27 +0300

    add: some styles

--- src/components/elements/PreTestText.tsx
+++ src/components/elements/PreTestText.tsx
@@ -2,6 +2,7 @@ import Button from "../common/Button";
 import "./PreTestText.scss";
 // import RocketImg from "../../assets/images/shuttle.png";
 import { TestNames } from "../../enums/testNames";
+import { useState } from "react";
 
 interface IPreTestText {
   testName: string;
@@ -10,7 +11,7 @@ interface IPreTestText {
 }
 
 const textParagraphs: string[] = [
-  "Тесты здесь отличаются от тех, к которым мы привыкли. Эти тесты скорее похожи на билеты, контроль ответов на которые остаётся за студентом.",
+  "Он отличается от тех, к которым мы привыкли. Этот тест скорее похожи на билет, где контроль ответов остаётся за студентом.",
   "Когда появится вопрос, необходимо будет отметить знаешь ты ответ или нет. Хорошо подумай и ответь сам себе как можно более развёрнуто, вспомни каке-то детали, которые относятся к вопросу.",
   "После выбора появится блок содержащий ответ на главный вопрос. Если ты понимаешь, что ответил неверно или хотел бы повторить этот вопрос в конце, можно поменять ответ. И вот зачем:",
   "После прохождения теста я покажу результат на основе твоего выбора. Ниже выведу все вопросы, в которых была “допущена ошибка” и прикреплю ссылку на эту тему.",
@@ -21,13 +22,20 @@ const textParagraphs: string[] = [
 const amountValues: number[] = [15, 30, 45, 60];
 
 function PreTestText({ testName, handleClick, onSetAmount }: IPreTestText) {
+  const [isOpened, setOpened] = useState(false);
+
   const js = testName === TestNames.JavaScript;
   const react = testName === TestNames.React;
   // const redux = testName === TestNames.Redux;
 
+  const handleOpenRules = () => setOpened(!isOpened);
+
   const renderTextParagraphs = () =>
     textParagraphs.map((paragraph: string, index: number) => (
-      <p key={index}>{paragraph}</p>
+      <>
+        <p key={index}>{paragraph}</p>
+        <hr />
+      </>
     ));
 
   const renderAmountChoice = () =>
@@ -40,7 +48,18 @@ function PreTestText({ testName, handleClick, onSetAmount }: IPreTestText) {
 
   return (
     <>
-      <div className="PreTestText">{renderTextParagraphs()}</div>
+      <div className="PreTestText">
+        <button onClick={handleOpenRules}>
+          1. Ознакомься с правилами теста
+        </button>
+        {isOpened && (
+          <div className="PreTestText-Rules">{renderTextParagraphs()}</div>
+        )}
+      </div>
+      <p>2. Выбери необходимое количество вопросов:</p>
+      <div className="PreTest-Amount" onChange={(e) => onSetAmount(e)}>
+        {renderAmountChoice()}
+      </div>
       <p className="PreTestText-DocsInfo">
         Тест создан на основе документации с сайта{" "}
         <a
@@ -56,10 +75,6 @@ function PreTestText({ testName, handleClick, onSetAmount }: IPreTestText) {
           {js ? "JavaScript" : react ? "React" : "Redux"}
         </a>
       </p>
-      <p>Выбери необходимое количество вопросов:</p>
-      <div className="PreTest-Amount" onChange={(e) => onSetAmount(e)}>
-        {renderAmountChoice()}
-      </div>
       <Button title="Начать тест" onClick={handleClick} />
     </>
   );
